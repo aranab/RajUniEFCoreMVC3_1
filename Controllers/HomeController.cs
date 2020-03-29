5ea5e58aa714510ece25701407b69cbe8b1ee932
Md. Arafat Rahman Rana <aranab.dba@gmail.com>
2020-03-30 02:11:54 +0600

    Added sorting, filtering, and paging inside index action method of students controller

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,26 +1,42 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+using RajUniEFCoreMVC3_1.Data;
+using RajUniEFCoreMVC3_1.Models;
+using RajUniEFCoreMVC3_1.Models.SchoolViewModels;
 using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
-using Microsoft.AspNetCore.Mvc;
-using Microsoft.Extensions.Logging;
-using RajUniEFCoreMVC3_1.Models;
 
 namespace RajUniEFCoreMVC3_1.Controllers
 {
     public class HomeController : Controller
     {
+        private readonly SchoolContext _context;
         private readonly ILogger<HomeController> _logger;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(SchoolContext context, ILogger<HomeController> logger)
         {
+            _context = context;
             _logger = logger;
         }
 
         public IActionResult Index()
         {
             return View();
+        }
+
+        public async Task<ActionResult> About()
+        {
+            IQueryable<EnrollmentDateGroup> data = from student in _context.Students 
+                                                   group student by student.EnrollmentDate into dateGroup 
+                                                   select new EnrollmentDateGroup() 
+                                                   { 
+                                                       EnrollmentDate = dateGroup.Key, 
+                                                       StudentCount = dateGroup.Count()
+                                                   };
+            
+            return View(await data.AsNoTracking().ToListAsync());
         }
 
         public IActionResult Privacy()
